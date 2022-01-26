9ebda96d5675c032702312b3db197c1280b17daf
nameeshadp14 <nameeshadp14@gmail.com>
2022-01-26 12:15:28 +0530

    Added script and Readme.txt

---
+++ guessinggame.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+function user_input {
+
+echo "Please guess the number of files in the current directory"
+read number
+ 
+}
+
+user_input
+
+actual_number=$(ls -la | wc -l)
+
+while [[ $number -ne $actual_number ]]
+
+do
+
+  echo "Your guess is wrong"
+  
+  for (( i=$actual_number-2;i<=$actual_number+2;i++ )) 
+  do
+ 
+  if [[ $number -eq i ]]
+  then
+  
+  echo "Your guess is near..!" 
+  break
+ 
+  elif [[ $number -gt i && $number -le $actual_number+2 ]]
+  then
+
+  continue
+
+  else  
+
+  echo "Your guess is far..!"
+
+  break 
+
+  fi
+  done
+
+  user_input
+
+done
+echo "Congratulations...! , You're right the number of files are $number"
+
